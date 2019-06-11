c7e621ae2278af1166ee8841131588f0e3970be8
Erte148 <rt.pskov@gmail.com>
2019-06-11 06:47:50 +0300

    Update node.lua

--- node.lua
+++ node.lua
@@ -71,6 +71,13 @@ local function Screen()
         gl.translate(-screen_x, -screen_y)
         util.draw_correct(obj, 0, 0, content_w, content_h)
     end
+	
+    local function draw2(obj)
+        --gl.rotate(-screen_rot, 0, 0, 1)
+        --gl.translate(-screen_x, -screen_y)
+        util.draw_correct(obj, 0, 0, 1920, 1080)
+    end
+	
 
     return {
         update = update;
@@ -116,7 +123,7 @@ tick2 = function(self, now)
 
         self.obj:start()
         local state, w, h = self.obj:state()
-        screen.draw(self.obj)
+        screen.draw2(self.obj)
         
 end;
     tick = function(self, now)
