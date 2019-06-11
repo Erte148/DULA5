825b1401e252f361412ab07987e1ee8cd57a917a
Erte148 <rt.pskov@gmail.com>
2019-06-11 03:45:52 +0300

    Update node.lua

--- node.lua
+++ node.lua
@@ -293,6 +293,160 @@ util.file_watch("playlist/config.json", function(raw)
 end)
 
 
+node.set_flag("slow_gc", false)
+
+local iblib = require "iblib"
+
+setmetatable(_G, {
+    __newindex = function(t, k, v)
+        error("cannot assign " .. k)
+    end
+})
+
+local function make_blender(blend_src)
+    local function create_shader(main_src)
+        local src = [[
+            uniform sampler2D Texture;
+            varying vec2 TexCoord;
+            uniform vec4 Color;
+            uniform float progress;
+
+            float blend(float x) {
+                ]] .. blend_src .. [[
+            }
+            void main() {
+                ]] .. main_src .. [[
+            }
+        ]]
+        return resource.create_shader(src)
+    end
+    local s1 = create_shader[[
+        gl_FragColor = texture2D(Texture, TexCoord) * vec4(1.0 - blend(progress));
+    ]]
+    local s2 = create_shader[[
+        gl_FragColor = texture2D(Texture, TexCoord) * vec4(blend(progress));
+    ]]
+    return function(c, n, progress, x1, y1, x2, y2)
+        s1:use{ progress = progress }
+        util.draw_correct(c, x1, y1, x2, y2)
+        s2:use{ progress = progress }
+        util.draw_correct(n, x1, y1, x2, y2)
+        s2:deactivate()
+    end
+end
+
+local faders = {
+    crossfade = function(c, n, progress, x1, y1, x2, y2)
+        util.draw_correct(c, x1, y1, x2, y2, 1.0 - progress)
+        util.draw_correct(n, x1, y1, x2, y2, progress)
+    end;
+
+    move = function(c, n, progress, x1, y1, x2, y2)
+        local xx = WIDTH * progress
+        util.draw_correct(c, x1 + xx, y1, x2 + xx, y2, 1.0 - progress)
+        util.draw_correct(n, x1 - WIDTH + xx, y1, x2 - WIDTH + xx, y2, progress)
+    end;
+
+    move_shrink = function(c, n, progress, x1, y1, x2, y2)
+        local xx = WIDTH * progress
+        util.draw_correct(c, x1 + xx, y1, x2, y2, 1.0 - progress)
+        util.draw_correct(n, x1 - WIDTH + xx, y1, x2 - WIDTH + xx, y2, progress)
+    end;
+
+    flip = function(c, n, progress, x1, y1, x2, y2)
+        local xx = WIDTH * progress
+        gl.pushMatrix()
+            gl.translate(WIDTH/2, HEIGHT/2)
+            gl.rotate(progress * 90, 0, 1, 0)
+            gl.translate(-WIDTH/2, -HEIGHT/2)
+            util.draw_correct(c, x1 + xx, y1, x2, y2, 1.0 - progress)
+        gl.popMatrix()
+
+        gl.pushMatrix()
+            gl.translate(WIDTH/2, HEIGHT/2)
+            gl.rotate(90 - progress * 90, 0, 1, 0)
+            gl.translate(-WIDTH/2, -HEIGHT/2)
+            util.draw_correct(n, x1 - WIDTH + xx, y1, x2 - WIDTH + xx, y2, progress)
+        gl.popMatrix()
+    end;
+
+    blend1 = make_blender[[
+        x = 1.0 - clamp(TexCoord.x - 1.0 + x * 3.0, 0.0, 1.0);
+        return 2.0 * x * x * x - 3.0 * x * x + 1.0;
+    ]],
+
+    blend2 = make_blender[[
+        x = 1.0 - clamp(TexCoord.y - 1.0 + x * 3.0, 0.0, 1.0);
+        return 2.0 * x * x * x - 3.0 * x * x + 1.0;
+    ]],
+
+    blend3 = make_blender[[
+        vec2 center = vec2(0.5, 0.5);
+        vec2 c = TexCoord - center;
+        float angle = atan(c.x, c.y) / 3.1415926536;
+        float dist = length(c);
+        x = abs(mod(angle + dist * 5.0 + x, 2.0) - 1.0) + dist - 2.0 + x * 4.0;
+        x = 1.0 - clamp(x, 0.0, 1.0);
+        return 2.0 * x * x * x - 3.0 * x * x + 1.0;
+    ]],
+
+    blend4 = make_blender[[
+        float y = sin( (TexCoord.x - 0.5) * x * 4.0) * sin( (TexCoord.y - 0.5) * x * 4.0);
+        return clamp(y - 1.0 + x * 4.0, 0.0, 1.0);  
+    ]],
+
+    blend5 = make_blender[[
+        return clamp(distance(TexCoord, vec2(0.5, 0.5)) - 1.0 + x * 3.0, 0.0, 1.0);
+    ]],
+
+    blend6 = make_blender[[
+        return 1.0 - (2.0 * x * x * x - 3.0 * x * x + 1.0);
+    ]],
+}
+
+local title_start = 99999999
+
+local idx = 0 -- offset before first item. will be incremented during first get_next_item
+
+local playlist_source2 = function()
+    return CONFIG.playlist2
+end;
+
+local overlay = resource.create_colored_texture(0, 0, 0, 1)
+
+local player2 = iblib.playlist{
+    get_next_item = function()
+        local playlist = playlist_source2()
+        idx = idx + 1
+        if idx > #playlist then
+            idx = 1
+        end
+        
+        local item = playlist[idx]
+        if not item then
+            return nil
+        else
+            return {
+                title = item.title;
+                duration = item.duration;
+                obj = item.file();
+            }
+        end
+    end;
+
+    get_switch_time = function()
+        return 0 --CONFIG.switch_time
+    end;
+
+    fade = function(...)
+        title_start = sys.now() + 1.0
+        return faders[0](...)
+    end;
+
+    draw = util.draw_correct;
+}
+
+
 
 
 
@@ -300,7 +454,7 @@ function node.render()
     gl.clear(0,0,0,1)
     if on then
       if vid then			
-    video3:draw(0, 0, WIDTH, HEIGHT)  			
+    player2.draw(0, 0, WIDTH, HEIGHT) 			
       else 
        video2:draw(0, 0, WIDTH, HEIGHT)          
      end       
