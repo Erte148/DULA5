cab9c398e27a505cf9c03ffb289f6dbb6b84abe0
Erte148 <rt.pskov@gmail.com>
2019-06-11 08:10:38 +0300

    Update node.lua

--- node.lua
+++ node.lua
@@ -75,7 +75,7 @@ local function Screen()
     local function drawq(obj)
         --gl.rotate(-screen_rot, 0, 0, 1)
         --gl.translate(-screen_x, -screen_y)
-        util.draw_correct(obj, 0, 0, 1920, 1080)
+        util.draw_correct(obj, 0, 0, NATIVE_WIDTH, NATIVE_HEIGHT)
     end
 	
 
