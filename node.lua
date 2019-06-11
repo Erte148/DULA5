9add22d0d35501ec118e199587e5d5802f79c592
Erte148 <rt.pskov@gmail.com>
2019-06-11 07:28:38 +0300

    Update node.lua

--- node.lua
+++ node.lua
@@ -358,7 +358,7 @@ function node.render()
     gl.clear(0,0,0,1)
     if on then
       if vid then                  
-    //playlist2.tick2(os.time())
+    --playlist2.tick2(os.time())
      	local idx = 1
         local item = items[idx]           
         item.state = "running"          
